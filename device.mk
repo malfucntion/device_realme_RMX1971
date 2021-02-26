c83722f36027ce1954680f3636a1fc661b0da0d1
Adam Lawson <adamjlawson22@gmail.com>
2021-02-26 17:37:53 +0000

    RMX1971: Switch to QTI BT stack

--- device.mk
+++ device.mk
@@ -101,6 +101,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl:32 \
     audio.bluetooth.default \
     libbthost_if \
+    libbluetooth_qti \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
